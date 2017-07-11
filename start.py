092195208d441e9f242dcb4dd38af96e4eb8a897
COLD-PLAY <mixishizi@163.com>
2017-07-11 22:38:47 +0800

    add start.py

---
+++ start.py
@@ -0,0 +1,27 @@
+import tensorflow as tf
+import numpy as np
+
+# create data
+x_data = np.random.rand(100).astype(np.float32)
+y_data = x_data*0.1 + 0.3
+
+### create tensorflow structure start ###
+weight = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+bias = tf.Variable(tf.zeros([1]))
+
+y = weight*x_data + bias
+
+loss = tf.reduce_mean(tf.square(y -  y_data))
+optimizer = tf.train.GradientDescentOptimizer(0.5)
+train = optimizer.minimize(loss)
+
+init = tf.global_variables_initializer()
+### create tensorflow structure end ###
+
+sess = tf.Session()
+sess.run(init)
+
+for step in range(201):
+	sess.run(train)
+	if step % 20 == 0:
+		print(step, sess.run(weight), sess.run(bias))
