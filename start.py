edd7368a42eed8c2e636e1bcf9518dbf6a0acbc7
廖舟 <mi_xi_shi_zi@163.com>
2017-10-15 21:08:38 +0800

    start learning...

--- start.py
+++ start.py
@@ -2,14 +2,18 @@ import tensorflow as tf
 import numpy as np
 
 # create data
-x_data = np.random.rand(100).astype(np.float32)
-y_data = x_data*0.1 + 0.3
+# x_data = np.random.rand(100).astype(np.float32)
+# y_data = x_data*0.1 + 0.3
+
+x_data = tf.float32(np.random.rand(2, 100))
+y_data = np.dot([0.100, 0.200], x_data) + 0.300
 
 ### create tensorflow structure start ###
-weight = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
 bias = tf.Variable(tf.zeros([1]))
+weight = tf.Variable(tf.random_uniform([1, 2], -1.0, 1.0))
 
-y = weight*x_data + bias
+# y = weight*x_data + bias
+y = tf.matmul(x_data, weight) + bias
 
 loss = tf.reduce_mean(tf.square(y -  y_data))
 optimizer = tf.train.GradientDescentOptimizer(0.5)
