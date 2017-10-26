83c7a3603e561ae18537f43a25cc8aee47fab29c
COLD-PLAY <mi_xi_shi_zi@163.com>
2017-10-26 10:05:40 +0800

    add more

--- sklearn_learn.py
+++ sklearn_learn.py
@@ -5,25 +5,28 @@ from sklearn.neighbors import KNeighborsClassifier
 
 import matplotlib.image as mpimg
 
-# iris = datasets.load_iris()
-# iris_X = iris.data
-# iris_y = iris.target
-
-faces = datasets.fetch_olivetti_faces()
-faces_data = faces.data
-
-houses = datasets.fetch_california_housing()
-houses_data = houses.data
+####################### this is an example for KNeigghborsClassifier to classify the iris(flowers)
+iris = datasets.load_iris()
+iris_X = iris.data
+iris_y = iris.target
 
 # print(iris_X[:2, :])
 # print(iris_y)
 
-# print(houses_data.shape)
-# print(houses_data[:2, :])
-# print(houses.image)
+X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.3)
+# print(y_train)
 
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
 
-# for i in range(len(houses_data)):
-#     print(i)
-#     filename = 'sklearn_data/houses/' + str(i) + '.jpg'
-#     mpimg.imsave(filename, houses_data[i, :].reshape(64, 64))
+prediction = knn.predict(X_test)
+print(prediction)
+print(y_test)
+
+err = 0
+num = len(y_test)
+for i in range(num):
+    if prediction[i] != y_test[i]:
+        err += 1
+
+print('error rate: ', str(err / num))
