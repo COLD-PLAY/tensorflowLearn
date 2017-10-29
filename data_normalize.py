89d0215814ac91d7e59bc7a4b7409c1ee9051a42
COLD-PLAY <mi_xi_shi_zi@163.com>
2017-10-30 00:09:10 +0800

    add more

---
+++ data_normalize.py
@@ -0,0 +1,43 @@
+from sklearn import preprocessing
+from sklearn.cross_validation import train_test_split
+from sklearn.datasets.samples_generator import make_classification
+from sklearn.svm import SVC
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+# a = np.array([[10, 2.7, 3.6],
+#               [-100, 5, -2],
+#               [120, 20, 40]], dtype=np.float64)
+# print(a)
+# print(preprocessing.scale(a))
+
+X, y = make_classification(n_samples=300, n_features=2, n_redundant=0, n_informative=2,
+                            random_state=22, n_clusters_per_class=1, scale=100)
+
+# plt.subplot(121)
+# plt.scatter(X[:, 0], X[:, 1], c=y)
+
+X = preprocessing.scale(X)
+
+# plt.subplot(122)
+# plt.scatter(X[:, 0], X[:, 1], c=y)
+
+# plt.show()
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+clf = SVC()
+# clf.fit(X_train, y_train)
+
+clf.fit(X, y)
+prediction = clf.predict(X)
+
+plt.subplot(121)
+plt.title('本来的分类')
+plt.scatter(X[:, 0], X[:, 1], c=y)
+
+plt.subplot(122)
+plt.title('预测的分类')
+plt.scatter(X[:, 0], X[:, 1], c=prediction)
+
+plt.show()
--- sklearn_learn_make_data.py
+++ sklearn_learn_make_data.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
 
 import matplotlib.pyplot as plt
 
@@ -13,13 +14,37 @@ import matplotlib.pyplot as plt
 # print(model.predict(data_X[:4]))
 # print(data_y[:4])
 
-X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=10.)
-plt.scatter(X, y)
+# X, y = datasets.make_regression(n_samples=100, n_features=1, n_targets=1, noise=10.)
+# plt.scatter(X, y)
+
+# model = LinearRegression()
+# model.fit(X, y)
+
+# print(model.coef_, model.intercept_) # y = W*X + b
+
+# # plt.scatter(X, model.coef_ * X + model.intercept_)
+# plt.plot(X, model.coef_ * X + model.intercept_, 'go')
+# plt.show()
+
+loaded_data = datasets.load_boston()
+data_X = loaded_data.data
+data_y = loaded_data.target
+train_X, test_X, train_y, test_y = train_test_split(data_X, data_y, test_size=0.3)
+
+# plt.plot(data_X, data_y, 'go')
+# plt.scatter(data_X, data_y)
+# plt.show()
 
 model = LinearRegression()
-model.fit(X, y)
+# model.fit(data_X, data_y)
+model.fit(train_X, train_y)
 
-print(model.coef_, model.intercept_) # y = W*X + b
+# print(model.coef_)
+# print(model.intercept_)
 
-plt.scatter(X, model.coef_ * X + model.intercept_)
-plt.show()
+# prediction = model.predict([data_X[0]])
+# print(prediction, data_y[0])
+
+print(model.get_params())
+# print(model.score(data_X, data_y))
+print(model.score(test_X, test_y))
