47bbd2f51d84577a9635a7f1c76dcedee0a7447b
COLD-PLAY <mi_xi_shi_zi@163.com>
2017-11-01 11:40:45 +0800

    learn cnn
    code cnn to classify mnist

--- mnist_with_cnn.py
+++ mnist_with_cnn.py
@@ -1,8 +1,6 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
-
 def compute_accuracy(v_xs, v_ys):
     global prediction
     y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1.})
@@ -15,16 +13,76 @@ def compute_accuracy(v_xs, v_ys):
 def weight_variable(shape):
     # Weights = tf.Variable(tf.random_normal(shape))
     # return Weights
-    initial = tf.
+    initial = tf.truncated_normal(shape, stddev=0.1)
+    return tf.Variable(initial)
 
 def bias_variable(shape):
-    bias = tf.Variable(tf.random_normal(tf.float32, shape) + 0.1)
-    return bias
+    # bias = tf.Variable(tf.random_normal(tf.float32, shape) + 0.1)
+    # return bias
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 def conv2d(x, W):
-    pass
+    # strides [1, x_movement, y_movement, 1]
+    # padding 'SAME' or 'VALID'
+    # MUST HAVE strides[0] = strides[3] = 1
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
 
 def max_pool_2x2(x):
-    pass
+    # strides [1, x_movement, y_movement, 1]
+    # padding 'SAME' or 'VALID'
+    # MUST HAVE strides[0] = strides[3] = 1
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-# define
+# define placeholder for inputs to CNN
+xs = tf.placeholder(tf.float32, [None, 784]) # 28 X 28
+ys = tf.placeholder(tf.float32, [None, 10])
+keep_prob = tf.placeholder(tf.float32)
+
+# the last 1 meams the height of the image, here is 1 because it's gray image
+# the first -1 means didn't care how many samples has
+x_image = tf.reshape(xs, [-1, 28, 28, 1])
+
+# print(x_image.shape) # [n_samples, 28, 28, 1]
+
+## covn1 layer ##
+W_conv1 = weight_variable([5, 5, 1, 32]) # patch 5x5, in size 1, out size 32
+b_conv1 = bias_variable([32])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
+h_pool1 = max_pool_2x2(h_conv1)                          # output size 14x14x32
+
+## covn2 layer ##
+W_conv2 = weight_variable([5, 5, 32, 64]) # patch 5x5, in size 1, out size 32
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv1)                          # output size 7x7x64
+
+## func1 layer ##
+W_fc1 = weight_variable([7*7*64, 1024])
+b_fc1 = bias_variable([1024])
+# [n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+## func2 layer ##
+W_fc2 = weight_variable([1024, 10])
+b_fc2 = bias_variable([10])
+# output the prediction, get the prediction
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
+
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+cross_entropy = -tf.reduce_mean(ys * tf.log(prediction))
+# train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+
+for _ in range(1001):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.3})
+    if _ % 50 == 0:
+        accuracy = compute_accuracy(mnist.test.images, mnist.test.labels)
+        print(_, accuracy)
