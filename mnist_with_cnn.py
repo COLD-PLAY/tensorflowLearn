72757c6bc8bf88ec178ca87b1ef3c6ac3d5b3bba
COLD-PLAY <mi_xi_shi_zi@163.com>
2017-11-02 08:36:17 +0800

    add

--- mnist_with_cnn.py
+++ mnist_with_cnn.py
@@ -54,8 +54,8 @@ h_pool1 = max_pool_2x2(h_conv1)                          # output size 14x14x32
 ## covn2 layer ##
 W_conv2 = weight_variable([5, 5, 32, 64]) # patch 5x5, in size 1, out size 32
 b_conv2 = bias_variable([64])
-h_conv2 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 14x14x64
-h_pool2 = max_pool_2x2(h_conv1)                          # output size 7x7x64
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)                          # output size 7x7x64
 
 ## func1 layer ##
 W_fc1 = weight_variable([7*7*64, 1024])
@@ -71,6 +71,7 @@ b_fc2 = bias_variable([10])
 # output the prediction, get the prediction
 prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
+
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
@@ -80,9 +81,10 @@ cross_entropy = -tf.reduce_mean(ys * tf.log(prediction))
 # train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
-for _ in range(1001):
+for _ in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
-    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.3})
+
+    sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
     if _ % 50 == 0:
-        accuracy = compute_accuracy(mnist.test.images, mnist.test.labels)
+        accuracy = compute_accuracy(mnist.test.images[:100], mnist.test.labels[:100])
         print(_, accuracy)
